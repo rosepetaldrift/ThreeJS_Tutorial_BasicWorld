b71e420915c24ba5f8c7b1abe314e2c9c2cae096
rosepetaldrift <rosesophiewright@gmail.com>
2022-04-21 18:35:06 -0500

    Basic Box ( cube mesh disabled )

--- main.js
+++ main.js
@@ -1,36 +1,105 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
+import * as THREE from 'https://cdn.skypack.dev/three@0.136';
 
-import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
+import {OrbitControls} from 'https://cdn.skypack.dev/three@0.136/examples/jsm/controls/OrbitControls.js';
+
+const DEFAULT_MASS = 10;
+
+
+class RigidBody {
+  constructor() {
+  }
+
+  setRestitution(val) {
+    this.body_.setRestitution(val);
+  }
+
+  setFriction(val) {
+    this.body_.setFriction(val);
+  }
+
+  setRollingFriction(val) {
+    this.body_.setRollingFriction(val);
+  }
+
+  createBox(mass, pos, quat, size) {
+    this.transform_ = new Ammo.btTransform();
+    this.transform_.setIdentity();
+    this.transform_.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
+    this.transform_.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
+    this.motionState_ = new Ammo.btDefaultMotionState(this.transform_);
+
+    const btSize = new Ammo.btVector3(size.x * 0.5, size.y * 0.5, size.z * 0.5);
+    this.shape_ = new Ammo.btBoxShape(btSize);
+    this.shape_.setMargin(0.05);
+
+    this.inertia_ = new Ammo.btVector3(0, 0, 0);
+    if (mass > 0) {
+      this.shape_.calculateLocalInertia(mass, this.inertia_);
+    }
+
+    this.info_ = new Ammo.btRigidBodyConstructionInfo(
+        mass, this.motionState_, this.shape_, this.inertia_);
+    this.body_ = new Ammo.btRigidBody(this.info_);
+
+    Ammo.destroy(btSize);
+  }
+
+  createSphere(mass, pos, size) {
+    this.transform_ = new Ammo.btTransform();
+    this.transform_.setIdentity();
+    this.transform_.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
+    this.transform_.setRotation(new Ammo.btQuaternion(0, 0, 0, 1));
+    this.motionState_ = new Ammo.btDefaultMotionState(this.transform_);
+
+    this.shape_ = new Ammo.btSphereShape(size);
+    this.shape_.setMargin(0.05);
+
+    this.inertia_ = new Ammo.btVector3(0, 0, 0);
+    if(mass > 0) {
+      this.shape_.calculateLocalInertia(mass, this.inertia_);
+    }
+
+    this.info_ = new Ammo.btRigidBodyConstructionInfo(mass, this.motionState_, this.shape_, this.inertia_);
+    this.body_ = new Ammo.btRigidBody(this.info_);
+  }
+}
 
 
 class BasicWorldDemo {
   constructor() {
-    this._Initialize();
   }
 
-  _Initialize() {
-    this._threejs = new THREE.WebGLRenderer({
+  initialize() {
+    this.collisionConfiguration_ = new Ammo.btDefaultCollisionConfiguration();
+    this.dispatcher_ = new Ammo.btCollisionDispatcher(this.collisionConfiguration_);
+    this.broadphase_ = new Ammo.btDbvtBroadphase();
+    this.solver_ = new Ammo.btSequentialImpulseConstraintSolver();
+    this.physicsWorld_ = new Ammo.btDiscreteDynamicsWorld(
+        this.dispatcher_, this.broadphase_, this.solver_, this.collisionConfiguration_);
+    this.physicsWorld_.setGravity(new Ammo.btVector3(0, -100, 0));
+
+    this.threejs_ = new THREE.WebGLRenderer({
       antialias: true,
     });
-    this._threejs.shadowMap.enabled = true;
-    this._threejs.shadowMap.type = THREE.PCFSoftShadowMap;
-    this._threejs.setPixelRatio(window.devicePixelRatio);
-    this._threejs.setSize(window.innerWidth, window.innerHeight);
+    this.threejs_.shadowMap.enabled = true;
+    this.threejs_.shadowMap.type = THREE.PCFSoftShadowMap;
+    this.threejs_.setPixelRatio(window.devicePixelRatio);
+    this.threejs_.setSize(window.innerWidth, window.innerHeight);
 
-    document.body.appendChild(this._threejs.domElement);
+    document.body.appendChild(this.threejs_.domElement);
 
     window.addEventListener('resize', () => {
-      this._OnWindowResize();
+      this.onWindowResize_();
     }, false);
 
     const fov = 60;
     const aspect = 1920 / 1080;
     const near = 1.0;
     const far = 1000.0;
-    this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    this._camera.position.set(75, 20, 0);
+    this.camera_ = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    this.camera_.position.set(75, 20, 0);
 
-    this._scene = new THREE.Scene();
+    this.scene_ = new THREE.Scene();
 
     let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
     light.position.set(20, 100, 10);
@@ -47,13 +116,13 @@ class BasicWorldDemo {
     light.shadow.camera.right = -100;
     light.shadow.camera.top = 100;
     light.shadow.camera.bottom = -100;
-    this._scene.add(light);
+    this.scene_.add(light);
 
     light = new THREE.AmbientLight(0x101010);
-    this._scene.add(light);
+    this.scene_.add(light);
 
     const controls = new OrbitControls(
-      this._camera, this._threejs.domElement);
+      this.camera_, this.threejs_.domElement);
     controls.target.set(0, 20, 0);
     controls.update();
 
@@ -66,74 +135,94 @@ class BasicWorldDemo {
         './resources/posz.jpg',
         './resources/negz.jpg',
     ]);
-    this._scene.background = texture;
+    //this.scene_.background = texture;
+    this.scene_.background = new THREE.Color( 0x89CFF0 );
 
-    const plane = new THREE.Mesh(
-        new THREE.PlaneGeometry(100, 100, 10, 10),
-        new THREE.MeshStandardMaterial({
-            color: 0xFFFFFF,
-          }));
-    plane.castShadow = false;
-    plane.receiveShadow = true;
-    plane.rotation.x = -Math.PI / 2;
-    this._scene.add(plane);
+    const ground = new THREE.Mesh(
+      new THREE.BoxGeometry(100, 1, 100),
+      new THREE.MeshStandardMaterial({color: 0x404040}));
+    ground.castShadow = false;
+    ground.receiveShadow = true;
+    this.scene_.add(ground);
+
+    const rbGround = new RigidBody();
+    rbGround.createBox(0, ground.position, ground.quaternion, new THREE.Vector3(100, 1, 100));
+    rbGround.setRestitution(0.99);
+    this.physicsWorld_.addRigidBody(rbGround.body_);
+
+    this.rigidBodies_ = [];
 
     const box = new THREE.Mesh(
-      new THREE.BoxGeometry(2, 2, 2),
-      new THREE.MeshStandardMaterial({
-          color: 0xFFFFFF,
-      }));
-    box.position.set(0, 1, 0);
+    new THREE.BoxGeometry(4, 4, 4),
+    new THREE.MeshStandardMaterial({color: 0x808080}));
+    box.position.set(0, 40, 0);
     box.castShadow = true;
     box.receiveShadow = true;
-    this._scene.add(box);
+    this.scene_.add(box);
 
-    for (let x = -8; x < 8; x++) {
-      for (let y = -8; y < 8; y++) {
-        const box = new THREE.Mesh(
-          new THREE.BoxGeometry(2, 2, 2),
-          new THREE.MeshStandardMaterial({
-              color: 0x808080,
-          }));
-        box.position.set(Math.random() + x * 5, Math.random() * 4.0 + 2.0, Math.random() + y * 5);
-        box.castShadow = true;
-        box.receiveShadow = true;
-        this._scene.add(box);
+    const rbBox = new RigidBody();
+    rbBox.createBox(1, box.position, box.quaternion, new THREE.Vector3(4, 4, 4));
+    rbBox.setRestitution(0.25);
+    rbBox.setFriction(1);
+    rbBox.setRollingFriction(5);
+    this.physicsWorld_.addRigidBody(rbBox.body_);
+    
+    this.rigidBodies_.push({mesh: box, rigidBody: rbBox});
+
+    let isSphere = true;
+
+    this.tmpTransform_ = new Ammo.btTransform();
+
+    this.countdown_ = 1.0;
+    this.count_ = 0;
+    this.previousRAF_ = null;
+    this.raf_();
+  }
+
+  onWindowResize_() {
+    this.camera_.aspect = window.innerWidth / window.innerHeight;
+    this.camera_.updateProjectionMatrix();
+    this.threejs_.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  raf_() {
+    requestAnimationFrame((t) => {
+      if (this.previousRAF_ === null) {
+        this.previousRAF_ = t;
       }
-    }
 
-    // const box = new THREE.Mesh(
-    //   new THREE.SphereGeometry(2, 32, 32),
-    //   new THREE.MeshStandardMaterial({
-    //       color: 0xFFFFFF,
-    //       wireframe: true,
-    //       wireframeLinewidth: 4,
-    //   }));
-    // box.position.set(0, 0, 0);
-    // box.castShadow = true;
-    // box.receiveShadow = true;
-    // this._scene.add(box);
-
-    this._RAF();
-  }
-
-  _OnWindowResize() {
-    this._camera.aspect = window.innerWidth / window.innerHeight;
-    this._camera.updateProjectionMatrix();
-    this._threejs.setSize(window.innerWidth, window.innerHeight);
-  }
-
-  _RAF() {
-    requestAnimationFrame(() => {
-      this._threejs.render(this._scene, this._camera);
-      this._RAF();
+      this.step_(t - this.previousRAF_);
+      this.threejs_.render(this.scene_, this.camera_);
+      this.raf_();
+      this.previousRAF_ = t;
     });
+  }
+
+    
+  step_(timeElapsed) {
+    const timeElapsedS = timeElapsed * 0.001;
+    this.physicsWorld_.stepSimulation(timeElapsedS, 10);
+
+    for (let i = 0; i < this.rigidBodies_.length; ++i) {
+      this.rigidBodies_[i].rigidBody.motionState_.getWorldTransform(this.tmpTransform_);
+      const pos = this.tmpTransform_.getOrigin();
+      const quat = this.tmpTransform_.getRotation();
+      const pos3 = new THREE.Vector3(pos.x(), pos.y(), pos.z());
+      const quat3 = new THREE.Quaternion(quat.x(), quat.y(), quat.z(), quat.w());
+
+      this.rigidBodies_[i].mesh.position.copy(pos3);
+      this.rigidBodies_[i].mesh.quaternion.copy(quat3);
+    }
   }
 }
 
 
-let _APP = null;
+let APP_ = null;
 
-window.addEventListener('DOMContentLoaded', () => {
-  _APP = new BasicWorldDemo();
+window.addEventListener('DOMContentLoaded', async () => {
+  Ammo().then((lib) => {
+    Ammo = lib;
+    APP_ = new BasicWorldDemo();
+    APP_.initialize();
+  });
 });
